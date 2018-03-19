23a87d504eb0a206d6cfc4e028a10b1a30ca9c8f
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-03-19 11:35:30 -0400

    test file for converting docx to doc to html

--- unoconv_test/php_unuconv_Test.php
+++ unoconv_test/php_unuconv_Test.php
@@ -1,3 +1,41 @@
+<?
+class phptest
+{
+private function cmd_exec($cmd, &$stdout, &$stderr)
+    {
+        $outfile        = tempnam(".", "cmd");
+        $errfile        = tempnam(".", "cmd");
+        $descriptorspec = array(
+            0 => array(
+                "pipe",
+                "r"
+            ),
+            1 => array(
+                "file",
+                $outfile,
+                "w"
+            ),
+            2 => array(
+                "file",
+                $errfile,
+                "w"
+            )
+        );
+        $proc           = proc_open($cmd, $descriptorspec, $pipes);
+
+        if (!is_resource($proc))
+            return 255;
+
+        fclose($pipes[0]); //Don't really want to give any input
+
+        $exit   = proc_close($proc);
+        $stdout = file($outfile);
+        $stderr = file($errfile);
+
+        unlink($outfile);
+        unlink($errfile);
+        return $exit;
+    }
 private function getNarrativebyWord($inputWordFile, $outputHtmlFile)
   {    /* uses cmd_exec to call java jar to convert word to html
        * call cmd_exec command and than return result
@@ -6,19 +44,25 @@ private function getNarrativebyWord($inputWordFile, $outputHtmlFile)
        * file_get_contents — Reads entire file into a string
        */
        $uniqout=uniqid("out");
-       $tempOutput = "../tools/$uniqout.doc";
-      $mime = mime_content_type($inputWordFile);
-      if ($mime == "application/msword")
-       {$this->cmd_exec('java -jar ../tools/wordtohtml.jar ' . $inputWordFile . '  ' . $outputHtmlFile, $returnvalue, $error);}
-      else if ($mime == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-         {$this->cmd_exec('unoconv -f doc -o ' . $tempOutput . '  ' .$inputWordFile, $returnvalue, $error);
+       $tempOutput = "$uniqout.doc";
+
+
+         $this->cmd_exec('unoconv -f doc -o ' . $tempOutput . '  ' .$inputWordFile, $returnvalue, $error);
              print_r($error);
              print_r($returnvalue);
-         }
-         {$this->cmd_exec('java -jar ../tools/wordtohtml.jar ' .  $tempOutput. '  ' . $outputHtmlFile, $returnvalue, $error);}
+
+        $this->cmd_exec('java -jar wordtohtml.jar ' .  $tempOutput. '  ' . $outputHtmlFile, $returnvalue, $error);
 
       $output = file_get_contents($outputHtmlFile, true);
       echo $output;
       unlink($tempOutput);
       return $output;
   }
+
+}
+$docxfile ="HNSC-PTEN.docx";
+$htmlfile ="test.html";
+$this->getNarrativebyWord($docxfile, $htmlfile);
+
+
+?>
